e0a9407d7c71b76d3286bc20b234d53f95bc950c
syfqy <syafiq.ysf@gmail.com>
2022-06-07 14:27:53 +0800

    add: while loop ex

---
+++ Day1/WhileLoop/WhileLoop.java
@@ -0,0 +1,15 @@
+import java.io.Console;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        Console cons = System.console();
+        String input = "ABC";
+        while(input.length() > 0) {
+            input = cons.readLine("Please enter some text. Enter blank to exit program: ");
+            if (input.length() > 0) {
+                System.out.printf("--- %s\n", input.toLowerCase());
+            }
+        }
+        System.out.println("BYE");
+    }
+}
